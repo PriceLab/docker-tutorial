ecb42799d6f89f2e0b76484b34f37ab54fdedc28
EC2 Default User <ec2-user@ip-10-0-0-89.us-west-2.compute.internal>
2018-09-13 17:19:27 +0000

    Added notes

--- beginner-tutorial/useful_servers/apache.sh
+++ beginner-tutorial/useful_servers/apache.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#see: https://hub.docker.com/_/httpd/
 set -x
 
 docker run -dit --name my-apache-app -p 8001:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
--- beginner-tutorial/useful_servers/shiny.sh
+++ beginner-tutorial/useful_servers/shiny.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+
+#see: https://hub.docker.com/r/rocker/shiny/
 set -x
 
 docker run --rm -p 8002:3838 rocker/shiny
