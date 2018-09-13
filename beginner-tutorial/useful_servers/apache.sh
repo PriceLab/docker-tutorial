2e031cbfaafde045ddfeed47d04b2d16d78b5a8f
John C. Earls <john.c.earls@gmail.com>
2018-09-13 08:38:39 -0700

    Adding shiny example

--- beginner-tutorial/useful_servers/apache.sh
+++ beginner-tutorial/useful_servers/apache.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -x
 
 docker run -dit --name my-apache-app -p 8001:80 -v "$PWD":/usr/local/apache2/htdocs/ httpd:2.4
---
+++ beginner-tutorial/useful_servers/shiny.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -x
+
+docker run --rm -p 8002:3838 rocker/shiny
